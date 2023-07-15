e09d8503bffcc13a9bb97fa9cb8ff0058cd2e877
saarasy <syifaauraarasy04@gmail.com>
2023-07-15 15:09:37 +0700

    initialize commit

--- index.php
+++ index.php
@@ -1,60 +1,7 @@
 <?php
-session_start();
-include "db.php";
-if (!isset($_SESSION['nama'])){
-	echo "
-	<script>window.location='login.php';</script>
-	";
-}
 
+//------------------------------::::::::::::::::::::------------------------------\\
+//------------------------------::::::::::::::::::::------------------------------\\
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PERPUSTAKAAN</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<div class="container">
-		<div class="kiri">
-			<ul><strong>
-				<a href="index.php?act=home"><li>Home</li></a>
-				<a href="index.php?act=buku"><li>Koleksi Buku</li></a>
-				<a href="index.php?act=peminjaman"><li>Peminjaman</li></a>
-				</strong>
-			</ul>
-		</div>
-		<div class="kanan">
-			<div class="hal">
-				<?php
-				if(isset($_GET['act'])){
-					if($_GET['act']=='home')
-						include 'home.php';
-					else
-						if($_GET['act']=='buku')
-							include 'buku.php';
-					else
-						if($_GET['act']=='peminjaman')
-							include 'peminjaman.php';
-				} else if (isset($_GET['editpinjam'])){
-					include "editpinjam.php";
-
-				} else include "depan.php";
-				
-
-				?>
-
-				
-			</div>
-		</div>
-	</div>
-	<div id="footer">
-			<div id="text">
-				<h5 style="color: white; text-align: center;"><br><br><br><br><br> PRODUCTS &nbsp; |&nbsp; BLOG &nbsp; |&nbsp; LIBRARY &nbsp; |&nbsp; CONTACT </h5>
-				<h5 style="text-align: center; color: white;">Coppyright @ 2023</h5>
-
-			</div>
-
-</body>
-</html>
+// Arahkan user ketika pertama kali membuka applikasi ke halaman login
+header("location: masuk");
--- register.php
+++ register.php
@@ -13,7 +13,7 @@ session_start();
     $sql = mysqli_query($koneksi, "SELECT * FROM identitas");
     $row1 = mysqli_fetch_assoc($sql);
     ?>
-    <title>Masuk | <?= $row1['nama_app']; ?></title>
+    <title>Pendaftaran | <?= $row1['nama_app']; ?></title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -42,19 +42,24 @@ session_start();
         <!-- /.login-logo -->
         <div class="login-box-body" style="border-radius: 10px;">
             <img src="assets/dist/img/logo_app.png" height="80px" width="80px" style="display: block; margin-left: auto; margin-right: auto; margin-top: -12px; margin-bottom: 5px;">
-            <form name="formLogin" action="function/Process.php?aksi=masuk" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
+
+            <form name="formLogin" action="function/Process.php?aksi=daftar" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
                 <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="username" id="username" placeholder="Nama Pengguna">
+                    <input type="text" class="form-control" name="funame" placeholder="Nama Lengkap" id="funame">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
                 <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="password" id="password" placeholder="Kata Sandi">
+                    <input type="text" class="form-control" name="uname" placeholder="Nama Pengguna" id="uname">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="passw" placeholder="Kata Sandi" id="passw">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 </div>
                 <div class="row">
                     <!-- /.col -->
                     <div class="col-xs-12">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Daftar Sekarang</button>
                     </div>
                     <!-- /.col -->
                 </div>
@@ -64,7 +69,7 @@ session_start();
                 <p style="font-size: 11px;">- ATAU -</p>
                 <div class="row">
                     <div class="col-xs-12">
-                        <button type="button" onclick="Register()" class="btn btn-block btn-warning btn-flat"><i class="fa fa-user-plus"></i> Daftar sebagai member</button>
+                        <button type="button" onclick="Masuk()" class="btn btn-block btn-success btn-flat"><i class="fa fa-sign-in"></i> Sudah mempunyai akun ? Masuk</button>
                     </div>
                 </div>
             </div>
@@ -80,94 +85,11 @@ session_start();
     <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap 3.3.7 -->
     <script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- -->
-    <script src="assets/json/lottie-player.js"></script>
-    <!-- Fungsi mengarahkan kehalaman pendaftaran -->
+    <!-- Fungsi mengarahkan kehalaman masuk -->
     <script>
-        function Register() {
-            window.location.href = "pendaftaran";
+        function Masuk() {
+            window.location.href = "masuk";
         }
-    </script>
-    <!-- Fungsi mengarahkan kehalaman lupa password -->
-    <script>
-        function ForgotPassword() {
-            window.location.href = "lupa-password";
-        }
-    </script>
-    <!-- Sweet Alert -->
-    <script src="assets/dist/js/sweetalert.min.js"></script>
-    <!-- Pesan Masuk Dulu -->
-    <script>
-        <?php
-        if (isset($_SESSION['masuk_dulu']) && $_SESSION['masuk_dulu'] <> '') {
-            echo "swal({
-                icon: 'error',
-                title: 'Peringatan',
-                text: '$_SESSION[masuk_dulu]',
-                buttons: false,
-                timer: 3000
-              })";
-        }
-        $_SESSION['masuk_dulu'] = '';
-        ?>
-    </script>
-    <!-- Pesan Pendaftaran -->
-    <script>
-        <?php
-        if (isset($_SESSION['berhasil']) && $_SESSION['berhasil'] <> '') {
-            echo "swal({
-                icon: 'success',
-                title: 'Berhasil',
-                text: '$_SESSION[berhasil]',
-                buttons: false,
-                timer: 3000
-              })";
-        }
-        $_SESSION['berhasil'] = '';
-        ?>
-    </script>
-    <script>
-        <?php
-        if (isset($_SESSION['gagal']) && $_SESSION['gagal'] <> '') {
-            echo "swal({
-                icon: 'error',
-                title: 'Peringatan',
-                text: '$_SESSION[gagal]',
-                buttons: false,
-                timer: 3000
-              })";
-        }
-        $_SESSION['gagal'] = '';
-        ?>
-    </script>
-    <!-- -->
-    <script>
-        <?php
-        if (isset($_SESSION['gagal_login']) && $_SESSION['gagal_login'] <> '') {
-            echo "swal({
-                icon: 'error',
-                title: 'Peringatan',
-                text: '$_SESSION[gagal_login]',
-                buttons: false,
-                timer: 3000
-              })";
-        }
-        $_SESSION['gagal_login'] = '';
-        ?>
-    </script>
-    <script>
-        <?php
-        if (isset($_SESSION['berhasil_keluar']) && $_SESSION['berhasil_keluar'] <> '') {
-            echo "swal({
-            icon: 'success',
-            title: 'Berhasil',
-            text: '$_SESSION[berhasil_keluar]',
-            buttons: false,
-            timer: 3000
-        })";
-        }
-        $_SESSION['berhasil_keluar'] = '';
-        ?>
     </script>
     <!-- Toastr -->
     <script src="assets/dist/js/toastr.min.js"></script>
@@ -194,14 +116,19 @@ session_start();
     <!-- -->
     <script>
         function validateForm() {
-            if (document.forms["formLogin"]["username"].value == "") {
-                toastr.error("Nama Pengguna harus diisi !!");
-                document.forms["formLogin"]["username"].focus();
+            if (document.forms["formLogin"]["funame"].value == "") {
+                toastr.error("Nama Lengkap harus diisi !!");
+                document.forms["formLogin"]["funame"].focus()
                 return false;
             }
-            if (document.forms["formLogin"]["password"].value == "") {
+            if (document.forms["formLogin"]["uname"].value == "") {
+                toastr.error("Nama Pengguna harus diisi !!");
+                document.forms["formLogin"]["uname"].focus();
+                return false;
+            }
+            if (document.forms["formLogin"]["passw"].value == "") {
                 toastr.error("Kata Sandi harus diisi !!");
-                document.forms["formLogin"]["password"].focus();
+                document.forms["formLogin"]["passw"].focus();
                 return false;
             }
         }
